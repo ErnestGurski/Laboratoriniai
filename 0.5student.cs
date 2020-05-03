a0a32c49d6caa33029d86bb7e6e614d002981ca4
ErnestGurski <64586209+ErnestGurski@users.noreply.github.com>
2020-05-03 21:52:43 +0300

    Update 0.5student.cs

--- 0.5student.cs
+++ 0.5student.cs
@@ -4,32 +4,8 @@ class student
     public string surname { get; set; }
     public double egzorez { get; set; }
     public double sum
-    {
-        get
-        { return this.vid * 0.3 + this.egzorez*0.7;  }
-    }
-
-    public double sumMed
-    {
-        get
-        {  return this.point[this.point.Count / 2] * 0.3 + this.egzorez*0.7;  }
-    }
-    public double vid
-    {
-        get
-        {
-            double temp = 0;
-            foreach (var value in this.point)
-            {
-                temp += value;
-            }
-            return temp / this.point.Count;
-        }
-    }
-
-    public List<double> point = new List<double>();
-
-
+    
+    
     public student(string name, string surname, double egzorez, List<double> point)
     {
         this.name = name;
